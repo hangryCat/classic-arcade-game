274d4056c493bed05090e1dfe5dce610cfeaad9f
Meo Huynh <mayoh727@gmail.com>
2018-08-20 18:48:02 -0700

    Change preset code variable from var to let

--- js/app.js
+++ js/app.js
@@ -234,7 +234,7 @@ allEnemies.push(enemy1, enemy2, enemy3, enemy4, enemy5);
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    let allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
